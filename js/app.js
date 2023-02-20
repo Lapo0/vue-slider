1cc09fa8d9ee66e96e058d883ad5ee0934db24b1
Lapo0 <lapo.gini.01@gmail.com>
2023-02-20 17:04:15 +0100

    FEAT class active or none

--- js/app.js
+++ js/app.js
@@ -10,43 +10,52 @@ const { createApp } = Vue
                       image: './img/01.webp',
                       title: "Marvel's Spiderman Miles Morale",
                       text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                      isActive: true,
                     },
                     {
                       image: './img/02.webp',
                       title: 'Ratchet & Clank: Rift Apart',
                       text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                      isActive: false,
                     },
                     {
                       image: './img/03.webp',
                       title: 'Fortnite',
                       text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+                      isActive: false,
                     },
                     {
                       image: './img/04.webp',
                       title: 'Stray',
                       text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                      isActive: false,
                     },
                     {
                       image: './img/05.webp',
                       title: "Marvel's Avengers",
                       text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+                    isActive: false,
                     },
                 ],
                 currentIndex: 0
             }
         },
         methods: {
+            updateSlideClasses(prevIndex, nextIndex) {
+              this.slides[prevIndex].isActive = false;
+              this.slides[nextIndex].isActive = true;
+            },
             nextSlide() {
-                const nextIndex = (this.currentIndex + 1) % this.slides.length;
-                this.updateSlideClasses(this.currentIndex, nextIndex);
-                this.currentIndex = nextIndex;
-              },
-              prevSlide() {
-                const prevIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
-                this.updateSlideClasses(this.currentIndex, prevIndex);
-                this.currentIndex = prevIndex;
-              },
-        },
+              const nextIndex = (this.currentIndex + 1) % this.slides.length;
+              this.updateSlideClasses(this.currentIndex, nextIndex);
+              this.currentIndex = nextIndex;
+            },
+            prevSlide() {
+              const prevIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+              this.updateSlideClasses(this.currentIndex, prevIndex);
+              this.currentIndex = prevIndex;
+            },
+          },
     }
     
 const app = createApp(option)
